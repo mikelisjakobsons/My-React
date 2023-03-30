0e854c12758a27e0e57d97b51286fdb45bf92a60
Michael Jacobson <48226221+mikelisjakobsons@users.noreply.github.com>
2023-03-30 19:40:04 -0400

    Add files via upload

---
+++ build/precache-manifest.4ddbb96560cf71fda305812cf241d7e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2258b0d8e26efade9251090c85d4813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5465c04b95283ebd5fb9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "340514824faec9aa9dd4",
+    "url": "/static/js/2.f604bc32.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f604bc32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5465c04b95283ebd5fb9",
+    "url": "/static/js/main.10a6d02a.chunk.js"
+  },
+  {
+    "revision": "2ac9830be748d356ded3",
+    "url": "/static/js/runtime-main.dddaef18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
